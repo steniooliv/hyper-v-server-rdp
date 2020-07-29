177b7a18a963c7b9642fe09a9ff9c7c246c3f9b1
Stenio Oliveira <steniooliv@gmail.com>
2020-07-29 09:16:55 -0300

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -4,3 +4,10 @@ Get NetConnectionProfile
 # Set Network interface Private, change number of InterfaceIndex
 Get NetConnectionProfile -InterfaceIndex 6 -NetworkCategory Private
 
+##
+
+# Set allow on firewall
+netsh.exe advfirewall firewall add rule name="ICMP ECHOREQUEST" dir=in protocol=icmpv4 action=allow
+
+# For block this rule
+netsh.exe advfirewall firewall add rule name="ICMP ECHOREQUEST" dir=in protocol=icmpv4 action=block
