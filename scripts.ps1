22a8c86e524c16f8004e52f53276847e3cd928ca
Stenio Oliveira <steniooliv@gmail.com>
2020-07-29 09:01:35 -0300

    Create scripts.ps1

---
+++ scripts.ps1
@@ -0,0 +1,6 @@
+# Get Network interface information
+Get NetConnectionProfile
+
+# Set Network interface Private, change number of InterfaceIndex
+Get NetConnectionProfile -InterfaceIndex 6 -NetworkCategory Private
+
